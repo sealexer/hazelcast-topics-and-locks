65a4070096f5d8891432a7d7ca3d7c591b020594
Alexander Shusherov <Alexander.Shusherov@netcracker.com>
2019-03-12 12:16:50 +0300

    Send objects to the topic asynchronously.

--- src/main/java/com/alex/test/hazelcast/controllers/TopicController.java
+++ src/main/java/com/alex/test/hazelcast/controllers/TopicController.java
@@ -5,6 +5,7 @@ import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.ITopic;
 import com.hazelcast.core.ReplicatedMap;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,9 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TopicController {
 
-
   private static final Logger log = LoggerFactory.getLogger(TopicController.class);
   private static final int NUMBER_OF_ITERATIONS = 100;
+  protected static final int SENDING_PERIOD_MILLIS = 500;
 
   private final HazelcastInstance hazelcastInstance;
   private final AtomicInteger counter = new AtomicInteger();
@@ -32,11 +33,23 @@ public class TopicController {
   @RequestMapping(value = "send", method = RequestMethod.GET)
   public String sendToTopic() {
 
-    for (int i = 0; i < NUMBER_OF_ITERATIONS; i++) {
-      sendOneObject(counter.getAndIncrement());
-    }
+    new Thread(() -> {
+      for (int i = 0; i < NUMBER_OF_ITERATIONS; i++) {
+        sendOneObject(counter.getAndIncrement());
+        sleep(SENDING_PERIOD_MILLIS);
+      }
+    }).start();
 
-    return "OK";
+    return String.format("OK. Will send %1d objects in %2d seconds.",
+        NUMBER_OF_ITERATIONS, TimeUnit.MILLISECONDS.toSeconds(NUMBER_OF_ITERATIONS * SENDING_PERIOD_MILLIS));
+  }
+
+  private void sleep(long millis) {
+    try {
+      Thread.sleep(millis);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   private void sendOneObject(int i) {
